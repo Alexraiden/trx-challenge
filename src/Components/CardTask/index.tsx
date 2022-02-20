8501ba71220f2d6caabf40428777e44d6b21ed53
carrillo alejandro <a.carrillo@inextenso.digital>
2022-02-20 15:07:43 +0100

    feat: working dragNdrop between columns

--- src/Components/CardTask/index.tsx
+++ src/Components/CardTask/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { ReactNode } from 'react'
+import { Draggable } from 'react-beautiful-dnd'
 import './style.css'
 
 export type CardTaskType = {
@@ -11,12 +13,23 @@ export type CardTaskType = {
 
 export const CardTask = (props: CardTaskType) => {
   console.log('props= ', props)
-  const { title, body } = props
+  const { title, body, index, id } = props
 
   return (
-    <div className="card">
-      <div className="cardHeader">{title}</div>
-      <div className="cardBody">{body}</div>
-    </div>
+    <Draggable draggableId={id} index={index!}>
+      {provided => (
+        <div
+          className="card"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className="card">
+            <div className="cardHeader">{title}</div>
+            <div className="cardBody">{body}</div>
+          </div>
+        </div>
+      )}
+    </Draggable>
   )
 }
--- src/Components/Layout/Dashboard/index.tsx
+++ src/Components/Layout/Dashboard/index.tsx
@@ -1,23 +1,81 @@
 import React, { useState } from 'react'
 import './style.css'
+import { DragDropContext, DropResult } from 'react-beautiful-dnd'
 import { Column } from 'Components/Column'
 import { columnsMock } from 'Mocks'
 
 export const Dashboard = () => {
-
   const [columns, setColumns] = useState(() => columnsMock)
-  
+
+  const onDragEnd = ({ source, destination }: DropResult) => {
+    // Cases no need to update columns
+    if (destination === undefined || destination === null) return null
+
+    if (
+      source.droppableId === destination.droppableId &&
+      destination.index === source.index
+    )
+      return null
+
+    const start = columns[source.droppableId]
+    const end = columns[destination.droppableId]
+
+    // Case card move in same column
+    if (start === end) {
+      const newList = start.cardList.filter(
+        (_: any, idx: number) => idx !== source.index,
+      )
+
+      newList.splice(destination.index, 0, start.cardList[source.index])
+
+      const newCol = {
+        ...start,
+        cardList: newList,
+      }
+
+      setColumns(state => ({ ...state, [newCol.id]: newCol }))
+      return null
+    }
+
+    // Case card move between columns
+    const newStartList = start.cardList.filter(
+      (_: any, idx: number) => idx !== source.index,
+    )
+
+    const newStartCol = {
+      ...start,
+      cardList: newStartList,
+    }
+
+    const newEndList = end.cardList
+
+    newEndList.splice(destination.index, 0, start.cardList[source.index])
+
+    const newEndCol = {
+      ...end,
+      cardList: newEndList,
+    }
+
+    setColumns(state => ({
+      ...state,
+      [newStartCol.id]: newStartCol,
+      [newEndCol.id]: newEndCol,
+    }))
+    return null
+  }
 
   return (
-    <div className="dashboard">
-      <div className="header">header</div>
-      <div className="body">
-        {Object.values(columns).map(col => {
-          const { id, title, cardList } = col
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="dashboard">
+        <div className="header">header</div>
+        <div className="body">
+          {Object.values(columns).map(col => {
+            const { id, title, cardList } = col
 
-          return <Column {...{ id, title, cardList }} key={col.id} />
-        })}
+            return <Column {...{ id, title, cardList }} key={col.id} />
+          })}
+        </div>
       </div>
-    </div>
+    </DragDropContext>
   )
 }
